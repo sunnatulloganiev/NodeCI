acb7f4dce06283aae0e15196819c1e1bc11c6b80
Sunnatullo Ganiev <sunnatulloganiev@MacBook-Pro.local>
2023-02-27 09:10:32 +0500

    minor changes

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '77880286135-rsg00r6eu0mrtr770eo6ejrdse2u0v5j.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+};
